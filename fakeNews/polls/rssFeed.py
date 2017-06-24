f0fe953c52644c2e6d9f2412dde011a3ed5d99df
Sanchez-Marchan Manuel <sanchez-marm@br-automation.co.at>
2017-06-24 17:27:10 +0900

    Added csv generation of news

--- fakeNews/polls/rssFeed.py
+++ fakeNews/polls/rssFeed.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 import feedparser
+import pandas as pd
+import numpy as np
 
 def readNews(request):
 	d = feedparser.parse('http://rss.cnn.com/rss/cnn_latest.rss')
-	print(d)
+	df = pd.DataFrame(d.entries)
+	filename = 'news.csv'
+	df.to_csv(filename, index=False, encoding='utf-8')
 	return HttpResponse("The news has been read")
