0136babad40988471d29fc8ea9d2d64ab229d3b5
Sanchez-Marchan Manuel <sanchez-marm@br-automation.co.at>
2017-06-25 14:12:43 +0900

    Added ML module to the webApp

--- fakeNews/polls/AICore.py
+++ fakeNews/polls/AICore.py
@@ -9,7 +9,7 @@ from __future__ import print_function
 import argparse
 import sys
 import tempfile
-
+import numpy as np
 from six.moves import urllib
 
 import pandas as pd
@@ -27,99 +27,47 @@ CATEGORICAL_COLUMNS = ["author", "language", "site_url", "country"]
 					   
 CONTINUOUS_COLUMNS = ["year", "month", "day", "hour", "minute", "txt_words", "txt_upper", 
 					  "txt_exclamation", "title_words", "title_upper", "title_exclamation"]
-
-# NOT USED (Files loaded from file)
-def maybe_download(train_data, test_data):
-  """Maybe downloads training data and returns train and test file names."""
-  if train_data:
-    train_file_name = train_data
-  else:
-    train_file = tempfile.NamedTemporaryFile(delete=False)
-    urllib.request.urlretrieve("http://mlr.cs.umass.edu/ml/machine-learning-databases/adult/adult.data", train_file.name)  # pylint: disable=line-too-long
-    train_file_name = train_file.name
-    train_file.close()
-    print("Training data is downloaded to %s" % train_file_name)
-
-  if test_data:
-    test_file_name = test_data
-  else:
-    test_file = tempfile.NamedTemporaryFile(delete=False)
-    urllib.request.urlretrieve("http://mlr.cs.umass.edu/ml/machine-learning-databases/adult/adult.test", test_file.name)  # pylint: disable=line-too-long
-    test_file_name = test_file.name
-    test_file.close()
-    print("Test data is downloaded to %s" % test_file_name)
-  return train_file_name, test_file_name
+					  
+"""Build an estimator."""
+# Sparse base columns.
+author 		= tf.contrib.layers.sparse_column_with_hash_bucket("author", hash_bucket_size=10000)
+language	= tf.contrib.layers.sparse_column_with_hash_bucket("language", hash_bucket_size=100)
+site_url	= tf.contrib.layers.sparse_column_with_hash_bucket("site_url", hash_bucket_size=10000)
+country		= tf.contrib.layers.sparse_column_with_hash_bucket("country", hash_bucket_size=100)
   
-def build_estimator(model_dir, model_type):
-  """Build an estimator."""
-  # Sparse base columns.
-  author 	= tf.contrib.layers.sparse_column_with_hash_bucket("author", hash_bucket_size=10000)
-  language	= tf.contrib.layers.sparse_column_with_hash_bucket("language", hash_bucket_size=100)
-  site_url	= tf.contrib.layers.sparse_column_with_hash_bucket("site_url", hash_bucket_size=10000)
-  country	= tf.contrib.layers.sparse_column_with_hash_bucket("country", hash_bucket_size=100)
-  
-  # Continuous base columns.
-  year 				= tf.contrib.layers.real_valued_column("year")
-  month 			= tf.contrib.layers.real_valued_column("month")
-  day 				= tf.contrib.layers.real_valued_column("day")
-  hour 				= tf.contrib.layers.real_valued_column("hour")
-  minute 			= tf.contrib.layers.real_valued_column("minute")
-  txt_words 		= tf.contrib.layers.real_valued_column("txt_words")
-  txt_upper 		= tf.contrib.layers.real_valued_column("txt_upper")
-  txt_exclamation	= tf.contrib.layers.real_valued_column("txt_exclamation")
-  title_words 		= tf.contrib.layers.real_valued_column("title_words")
-  title_upper 		= tf.contrib.layers.real_valued_column("title_upper")
-  title_exclamation	= tf.contrib.layers.real_valued_column("title_exclamation")
+# Continuous base columns.
+year 			= tf.contrib.layers.real_valued_column("year")
+month 			= tf.contrib.layers.real_valued_column("month")
+day 			= tf.contrib.layers.real_valued_column("day")
+hour 			= tf.contrib.layers.real_valued_column("hour")
+minute 			= tf.contrib.layers.real_valued_column("minute")
+txt_words 		= tf.contrib.layers.real_valued_column("txt_words")
+txt_upper 		= tf.contrib.layers.real_valued_column("txt_upper")
+txt_exclamation	= tf.contrib.layers.real_valued_column("txt_exclamation")
+title_words 		= tf.contrib.layers.real_valued_column("title_words")
+title_upper 		= tf.contrib.layers.real_valued_column("title_upper")
+title_exclamation	= tf.contrib.layers.real_valued_column("title_exclamation")
 
-  # Wide columns and deep columns.
-  # wide_columns = [gender, native_country, education, occupation, workclass,
-                  # relationship, age_buckets,
-                  # tf.contrib.layers.crossed_column([education, occupation],
-                                                   # hash_bucket_size=int(1e4)),
-                  # tf.contrib.layers.crossed_column(
-                      # [age_buckets, education, occupation],
-                      # hash_bucket_size=int(1e6)),
-                  # tf.contrib.layers.crossed_column([native_country, occupation],
-                                                   # hash_bucket_size=int(1e4))]
-  wide_columns = [author, language, site_url, country, year, month, day, hour, minute, txt_words,
-                  txt_upper, txt_exclamation, title_words, title_upper, title_exclamation]
-  deep_columns = [
-      tf.contrib.layers.embedding_column(author, 	dimension=8),
-      tf.contrib.layers.embedding_column(language, 	dimension=8),
-      tf.contrib.layers.embedding_column(site_url, 	dimension=8),
-      tf.contrib.layers.embedding_column(country, 	dimension=8),
-      year,
-      month,
-	  day,
-      hour,
-      minute,
-      txt_words,
-	  txt_upper,
-	  txt_exclamation,
-	  title_words,
-	  title_upper,
-	  title_exclamation]
-
-  if model_type == "wide":
-    m = tf.contrib.learn.LinearClassifier(feature_columns=wide_columns,
-										  optimizer=tf.train.FtrlOptimizer(
-											learning_rate=0.1,
-											l1_regularization_strength=1.0,
-											l2_regularization_strength=1.0),
-										  model_dir=model_dir)
-  elif model_type == "deep":
-    m = tf.contrib.learn.DNNClassifier(model_dir=model_dir,
-                                       feature_columns=deep_columns,
-                                       hidden_units=[100, 50])
-  else:
-    m = tf.contrib.learn.DNNLinearCombinedClassifier(
-        model_dir=model_dir,
-        linear_feature_columns=wide_columns,
-        dnn_feature_columns=deep_columns,
-        dnn_hidden_units=[100, 50],
-        fix_global_step_increment_bug=True)
-  return m
-
+wide_columns = [author, language, site_url, country, year, month, day, hour, minute, txt_words,
+			  txt_upper, txt_exclamation, title_words, title_upper, title_exclamation]
+			  
+deep_columns = [
+  tf.contrib.layers.embedding_column(author, 	dimension=8),
+  tf.contrib.layers.embedding_column(language, 	dimension=8),
+  tf.contrib.layers.embedding_column(site_url, 	dimension=8),
+  tf.contrib.layers.embedding_column(country, 	dimension=8),
+  year,
+  month,
+  day,
+  hour,
+  minute,
+  txt_words,
+  txt_upper,
+  txt_exclamation,
+  title_words,
+  title_upper,
+  title_exclamation]
+		
 def input_fn(df):
   """Input builder function."""
   # Creates a dictionary mapping from each continuous feature column name (k) to
@@ -141,83 +89,82 @@ def input_fn(df):
   # Returns the feature columns and the label.
   return feature_cols, label
 
-def train_and_eval(model_dir, model_type, train_steps, train_data, test_data):
-  """Train and evaluate the model."""
-  # train_file_name, test_file_name = maybe_download(train_data, test_data)
-  train_file_name = "C:\\Users\\pavanc\\Desktop\\DataSet\\TrainSetFakeNews.csv"
-  test_file_name = "C:\\Users\\pavanc\\Desktop\\DataSet\\TestSetFakeNews.csv"
-  df_train = pd.read_csv(
-      tf.gfile.Open(train_file_name),
-      names=COLUMNS,
-      skipinitialspace=True,
-      engine="python",
-	  error_bad_lines=False)
-  df_test = pd.read_csv(
-      tf.gfile.Open(test_file_name),
-      names=COLUMNS,
-      skipinitialspace=True,
-      skiprows=1,
-      engine="python",
-	  error_bad_lines=False)
+"""Train and evaluate the model."""
+# train_file_name, test_file_name = maybe_download(train_data, test_data)
+train_file_name = "C:\\Users\\pavanc\\Desktop\\DataSet\\TrainSetNew.csv"
+test_file_name = "C:\\Users\\pavanc\\Desktop\\DataSet\\TestSetNew.csv"
 
-  # remove NaN elements
-  df_train = df_train.dropna(how='any', axis=0)
-  df_test = df_test.dropna(how='any', axis=0)
-
-  df_train[LABEL_COLUMN] = (df_train["type"].apply(lambda x: "fake" in x)).astype(int)
-  df_test[LABEL_COLUMN]  = (df_test["type"].apply(lambda x: "fake" in x)).astype(int)
-
-  model_dir = tempfile.mkdtemp() if not model_dir else model_dir
-  print("model directory = %s" % model_dir)
-
-  m = build_estimator(model_dir, model_type)
+df_train = pd.read_csv(
+  tf.gfile.Open(train_file_name),
+  names=COLUMNS,
+  skipinitialspace=True,
+  engine="python",
+  encoding="utf_8",
+  error_bad_lines=False)
   
-  m.fit(input_fn=lambda: input_fn(df_train), steps=train_steps)
+df_test = pd.read_csv(
+  tf.gfile.Open(test_file_name),
+  names=COLUMNS,
+  skipinitialspace=True,
+  engine="python",
+  encoding="utf_8",
+  error_bad_lines=False)
+
+# remove NaN elements
+df_train = df_train.dropna(how='any', axis=0)
+df_test = df_test.dropna(how='any', axis=0)
+
+df_train[LABEL_COLUMN] = (df_train["type"].apply(lambda x: "fake" in x)).astype(int)
+df_test[LABEL_COLUMN]  = (df_test["type"].apply(lambda x: "fake" in x)).astype(int)
+
+# Build model
+model_dir = "C:\\Users\\pavanc\\Desktop\\DataSet\\Model"
+print("model directory = %s" % model_dir)
+ 
+m = tf.contrib.learn.DNNLinearCombinedClassifier(
+	model_dir=model_dir,
+	linear_feature_columns=wide_columns,
+	dnn_feature_columns=deep_columns,
+	dnn_hidden_units=[100, 50],
+	fix_global_step_increment_bug=True)
+
+# train_steps	= 200
+# m.fit(input_fn=lambda: input_fn(df_train), steps=train_steps)
+
+results = m.evaluate(input_fn=lambda: input_fn(df_test), steps=1)
+
+for key in sorted(results):
+  print("%s: %s" % (key, results[key]))
+ 
+# # Predict  
+# predict_file_name = "C:\\Users\\pavanc\\Desktop\\DataSet\\NewSample.csv"
+# new_sample =  pd.read_csv(
+  # tf.gfile.Open(predict_file_name),
+  # names=COLUMNS,
+  # skipinitialspace=True,
+  # engine="python",
+  # encoding="utf_8",
+  # error_bad_lines=False)
+ 
+# """Input builder function."""
+# # Creates a dictionary mapping from each continuous feature column name (k) to
+# # the values of that column stored in a constant Tensor.
+# continuous_cols = {k: tf.constant(new_sample[k].values) for k in CONTINUOUS_COLUMNS}
+# # Creates a dictionary mapping from each categorical feature column name (k)
+# # to the values of that column stored in a tf.SparseTensor.
+# categorical_cols = {
+  # k: tf.SparseTensor(
+	  # indices=[[i, 0] for i in range(new_sample[k].size)],
+	  # values=new_sample[k].values,
+	  # dense_shape=[new_sample[k].size, 1])
+  # for k in CATEGORICAL_COLUMNS}
+# # Merges the two dictionaries into one.
+# feature_cols = dict(continuous_cols)
+# feature_cols.update(categorical_cols)
+# # Returns the feature columns and the label.
   
-  results = m.evaluate(input_fn=lambda: input_fn(df_test), steps=1)
-  
-  for key in sorted(results):
-    print("%s: %s" % (key, results[key]))
+# prediction = list(m.predict(input_fn = feature_cols))
 
-FLAGS = None
-
-def main(_):
-  train_and_eval(FLAGS.model_dir, FLAGS.model_type, FLAGS.train_steps,
-                 FLAGS.train_data, FLAGS.test_data)
-
-
-if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.register("type", "bool", lambda v: v.lower() == "true")
-  parser.add_argument(
-      "--model_dir",
-      type=str,
-      default="",
-      help="Base directory for output models."
-  )
-  parser.add_argument(
-      "--model_type",
-      type=str,
-      default="wide_n_deep",
-      help="Valid model types: {'wide', 'deep', 'wide_n_deep'}."
-  )
-  parser.add_argument(
-      "--train_steps",
-      type=int,
-      default=200,
-      help="Number of training steps."
-  )
-  parser.add_argument(
-      "--train_data",
-      type=str,
-      default="",
-      help="Path to the training data."
-  )
-  parser.add_argument(
-      "--test_data",
-      type=str,
-      default="",
-      help="Path to the test data."
-  )
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+# print(
+    # "New Samples, Class Predictions:    {}\n"
+    # .format(prediction))
--- fakeNews/polls/rssFeed.py
+++ fakeNews/polls/rssFeed.py
@@ -3,14 +3,102 @@ import feedparser
 import pandas as pd
 import numpy as np
 import colorsys
+import string
+from urllib.parse import urlparse
+import re
+from . import AICore
 
 def readNews(request):
+
+	#get news from RSS channel
 	d = feedparser.parse('http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml')
+	
+	#Parse the new one by one
+	i = 0
+	for i in range(len(d.entries)):
+		calcEntries = []
+		#adding author to list
+		if('author' in d.entries[i]):
+			calcEntries.append(d.entries[i].author)
+		else:
+			calcEntries.append(0)
+		# adding language
+		a = d.feed.language
+		checksum = len(a)
+		if(checksum > 2):
+			if('language' in d.feed):
+				a = d.feed.language.split('-')
+				
+				calcEntries.append(a[0])
+				calcEntries.append(a[1])
+			else:
+				
+				calcEntries.append(0)
+				calcEntries.append(0)	
+		
+		else:
+			if('language' in d.feed):
+				a = d.feed.language.split('-')
+				
+				calcEntries.append(a[0])
+				calcEntries.append(0)
+			else:
+				
+				calcEntries.append(0)
+				calcEntries.append(0)				
+		#adding url
+		if('link' in d.entries[i]):
+			a = urlparse(d.entries[i].link)
+			calcEntries.append(a.scheme+'://'+a.netloc)
+		else:
+			calcEntries.append(0)	
+		#adding year,month,day,hour,min
+		if('published_parsed' in d.entries[i]):
+			a = d.entries[i].published_parsed
+			calcEntries.append(d.entries[i].published_parsed.tm_year)
+			calcEntries.append(d.entries[i].published_parsed.tm_mon)
+			calcEntries.append(d.entries[i].published_parsed.tm_mday)
+			calcEntries.append(d.entries[i].published_parsed.tm_hour)
+			calcEntries.append(d.entries[i].published_parsed.tm_min)
+		else:
+			calcEntries.append(0)
+			calcEntries.append(0)
+			calcEntries.append(0)
+			calcEntries.append(0)
+			calcEntries.append(0)
+		#calculating number of words in text
+		if('summary' in d.entries[i]):
+			a = d.entries[i].summary 
+			calcEntries.append(len(a.split()))	
+			calcEntries.append(sum(1 for c in a if c.isupper()))
+			calcEntries.append(len(re.findall("!",a)))
+		else:
+			calcEntries.append(0)
+			calcEntries.append(0)
+			calcEntries.append(0)
+		#calculating number of words,!, and capital letters in title
+		if('title' in d.entries[i]):
+			a = d.entries[i].title 
+			calcEntries.append(len(a.split()))
+			calcEntries.append(sum(1 for c in a if c.isupper()))
+			calcEntries.append(len(re.findall("!",a)))
+		else:
+			calcEntries.append(0)
+			calcEntries.append(0)
+			calcEntries.append(0)
+		#Generate CSV File
+		df = pd.DataFrame(calcEntries)
+		filename = 'oneNew.csv'
+		df.to_csv(filename, index=False, encoding='utf-8')
+		#Call the model
+		
+	
+	#Get probability
 	for new in d.entries:
 		new.update({'probability' : str(round(np.random.uniform(0, 100), 2)) + '%'})
-	df = pd.DataFrame(d.entries)
-	filename = 'news.csv'
-	df.to_csv(filename, index=False, encoding='utf-8')
+	
+	
+	#Send news to Front-end
 	context = {'entries': d.entries}
 	return render(request, 'polls/index.html', context)
 	
